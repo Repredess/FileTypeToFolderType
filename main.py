b6cd23afdcea10b5a53bbd41232a7b29cfdc8bf2
Repredess <vverkir@gmail.com>
2023-03-18 20:50:57 +0300

    Add path-validation, recompilate Mac & Linux scripts to newest version

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os
 import time
 from datetime import datetime
 from watchdog.events import FileSystemEventHandler
+
+
 # import simplejson as json
 
 
@@ -15,7 +17,8 @@ class Handler(FileSystemEventHandler):
             extension = filename.split('.')[-1].lower()  # Получаем расширение файла
 
             # Если является файлом:
-            if os.path.isfile(os.path.join(folder_track, filename)) and not filename.startswith('.') and not extension in ['lnk', 'url']:
+            if os.path.isfile(os.path.join(folder_track, filename)) and not filename.startswith(
+                    '.') and not extension in ['lnk', 'url']:
 
                 # Ищем наше расширение:
                 for key, value in file_extensions.items():
@@ -29,7 +32,8 @@ class Handler(FileSystemEventHandler):
 
                         elif os.path.exists(os.path.join(folder_dest, key, filename)):
                             new_filename = str(datetime.now()).replace('.', '-')
-                            os.rename(os.path.join(folder_track, filename), (os.path.join(folder_track, f"{new_filename}.{extension}")))
+                            os.rename(os.path.join(folder_track, filename),
+                                      (os.path.join(folder_track, f"{new_filename}.{extension}")))
 
                         if new_filename:
                             file = os.path.join(folder_track, f"{new_filename}.{extension}")
@@ -53,15 +57,17 @@ class Handler(FileSystemEventHandler):
 
 file_extensions = {
     'Изображения': ['jpg', 'png', 'bmp', 'gif', 'tif', 'jpeg', 'ico'],
-    'Документы и электронные таблицы': ['xml', 'xlsx', 'xltx', 'xlt', 'tbl', 'docm', 'docx', 'xls', 'xsl', 'xltm', 'doc'],
+    'Документы и электронные таблицы': ['xml', 'xlsx', 'xltx', 'xlt', 'tbl', 'docm', 'docx', 'xls', 'xsl', 'xltm',
+                                        'doc'],
     'PDF-документы': ['pdf'],
     'Текстовые файлы': ['txt', 'text', 'tex', 'ttf', 'log', 'sub', 'apt', 'err', 'pwi'],
-    'Архивы': ['zip', 'rar', '7z', 'gzip', 'bin', 'jar'],
+    'Архивы': ['zip', 'rar', '7z', 'gzip', 'bin', 'jar', 'tar'],
     'Аудиофайлы': ['mp3', 'wav', 'midi', 'aac', 'flac', 'alac', 'aac', 'wav'],
     'Видеофайлы': ['mp4', 'avi', 'mkv', 'wmv', 'flv', 'mpeg', 'mow'],
     'Cтраницы из интернета': ['html', 'htm', 'mht'],
     'Презентации': ['ppt', 'pptx'],
-    'Файлы и базы данных': ['sql', 'dat', 'sav', 'mpp', 'csv', 'css', 'db', 'gbr', 'mdb', 'sqlite3', 'accdb', 'nbp', 'db3'],
+    'Файлы и базы данных': ['sql', 'dat', 'sav', 'mpp', 'csv', 'css', 'db', 'gbr', 'mdb', 'sqlite3', 'accdb', 'nbp',
+                            'db3'],
     'Установочные файлы': ['dmg', 'exe', 'pkg', 'msi'],
     'ISO-файлы, файлы образа диска': ['iso', 'gho', 'ghs'],
     'Торрент-файлы': ['torrent'],
@@ -72,6 +78,8 @@ file_extensions = {
     'Файлы проектов и резервных копий': ['prj', 'v2i', 'sis', 'sav', 'bak', 'dmp'],
     'JSON-файлы': ['json']
 }
+
+
 ##################################
 # Чтение нужных путей из json-файла:
 
@@ -91,10 +99,28 @@ file_extensions = {
 
 #################
 # Ручная настройка в терминале:
+def valid_path(txt=''):
+    path = input(f"{txt}\n").lstrip('\\')
+    if os.path.exists(path):
+        return path
+    elif path == "":
+        return ""
+    else:
+        return False
 
-print('Пример: C:\\Users\\Repredess\\Downloads')
-folder_track = input("Введите полный путь отслеживаемой папки: \n").lstrip('\\')
-folder_dest = input("Введите полный путь к папке для отсортированных файлов: \n").lstrip('\\')
+
+folder_track = valid_path(txt='Введите полный путь до "грязной" папки:')
+while not folder_track:
+    folder_track = valid_path(txt='Данный путь не существует в системе, введите корректный путь до "грязной" папки:')
+
+folder_dest = valid_path(txt='Введите полный путь до "чистой" папки(чтобы отсортировать в эту же папку: нажмите enter):')
+if folder_dest == '':
+    folder_dest = folder_track
+elif not folder_dest:
+    print("Данного пути не существует")
+    while folder_dest != '' or folder_dest == False:
+        folder_dest = valid_path(
+            txt='Введите полный путь до "чистой" папки(для сортировки в "грязную" папку: нажмите enter):')
 
 #################
 handle = Handler()
